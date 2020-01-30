5cfe02d783ab2416c40c332bfff3b6bf74d279ed
Pavlo Sakhanda <psakhanda@gmail.com>
2020-01-30 22:34:14 +0200

    Updates

---
+++ precache-manifest.0f909d75c93d941cd7ec7daa40d7c3b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5803afef8d02fb3283066df6a24eddca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "79e05131bde0f0ea183b",
+    "url": "/monsters-rolodex/static/css/main.0422b175.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79e05131bde0f0ea183b",
+    "url": "/monsters-rolodex/static/js/main.b01b774e.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
