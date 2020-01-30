ed8fd508f07cd51f1699164ed6ed0c13fb4a3353
Pavlo Sakhanda <psakhanda@gmail.com>
2020-01-30 22:35:59 +0200

    adding first react project to github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { CardList} from "./components/card-list/card-list.component";
+import { CardList} from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: []
-        }
+            monsters: [],
+            searchField: ' '
+        };
     }
 
     componentDidMount() {
@@ -16,10 +18,21 @@ class App extends Component {
             .then(users => this.setState({monsters: users}))
     }
 
+    onSearchChange = (e) => {
+        this.setState({searchField: e.target.value});
+    };
+
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
         return (
-            <div className="App">
-                <CardList monsters={this.state.monsters}/>
+            <div className='App'>
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeholder='search monsters'
+                    onSearchChange={this.onSearchChange}
+                />
+                <CardList monsters={filteredMonsters}/>
             </div>
         )
     }
